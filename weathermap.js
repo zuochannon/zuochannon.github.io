5bd49130604703d184b6f8ae151482b4b875ea08
Channon <channonz@Channons-MacBook-Pro.local>
2019-07-12 12:50:19 -0700

    added opacity changer (lower and higher)

--- weathermap.js
+++ weathermap.js
@@ -2,10 +2,14 @@ var view;
 var map;
 var initialLocation;
 var london;
+var newyord;
+var opacityStrength
 
 function init() {
-  initialLocation = ol.proj.fromLonLat([41.043316, 28.862457]); // [41.043316, 28.862457] africa coods? [40.730610, -73.935242]coords to NY
+  initialLocation = ol.proj.fromLonLat([41.043316, 28.862457]); // [41.043316, 28.862457] africa coods?
   london = ol.proj.fromLonLat([-0.12755, 51.507222]);
+  newyork = ol.proj.fromLonLat([40.7535949,-73.9947243]); //[40.730610, -73.935242]coords to NY
+  opacityStrength = 1.0
 
   view = new ol.View({
     center: initialLocation,
@@ -20,7 +24,8 @@ function init() {
        })
      ],
      loadTilesWhileAnimating: true,
-     view: view
+     view: view,
+     opacity: opacityStrength
    });
  }
 
@@ -28,18 +33,28 @@ function init() {
    view.animate({
      center: initialLocation,
      duration: 2000
-   });
+   })
  }
 
 function panLondon() {
   view.animate({
     center: london, // "London" location
     duration: 2000
-  });
+  })
 }
 
+function panNY() {
+  view.animate({
+    center: newyork
+  })
+}
 
+function opacityLower() {
+  document.getElementById("map").style.opacity = "0.5";//opacityStrength.toString(opacityStrength-.05)
+}
 
-
+function opacityHigher() {
+  document.getElementById("map").style.opacity = "1.0";
+}
 
 window.onload = init;
