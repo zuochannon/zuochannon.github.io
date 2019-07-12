b20842f7789cf25303cc3e90fa5a0ab44ef0e22b
Channon <channonz@Channons-MacBook-Pro.local>
2019-07-12 10:24:23 -0700

    made a js file for the map animation

---
+++ weathermap.js
@@ -0,0 +1,34 @@
+var view;
+var map;
+var initialLocation;
+var london;
+
+function init() {
+  initialLocation = ol.proj.fromLonLat([40.730610, -73.935242]);
+  london = ol.proj.fromLonLat([-0.12755, 51.507222]);
+
+  view: new ol.View({
+    center: initialLocation,
+    zoom: 4
+  });
+
+  map = new ol.Map({
+     target: 'map', // The "Target" is our <div> name
+     layers: [
+       new ol.layer.Tile({
+         source: new ol.source.OSM()
+       })
+     ],
+     loadTilesWhileAnimating: true,
+     view: view
+   });
+ }
+
+function panLondon() {
+  view.animatie({
+    center: london, // "London" location
+    duration: 2000
+  });
+}
+
+window.onload = init;
