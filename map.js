02bf6d3138bb921de73b883537487af11d02a795
Channon <channonz@Channons-MacBook-Pro.local>
2019-07-12 12:59:18 -0700

    changed map.html to js file

---
+++ map.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<!-- OPEN LAYERS API -->
+	<link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+	<style>
+		.map {
+			height: 400px;
+			width: 100%;
+		}
+	</style>
+
+	<script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+	<script src="weathermap.js"></script>
+
+	<title>Weather Map</title>
+
+
+
+</head>
+<body>
+
+	<p> <!-- paragraph marker added here for spacing -->
+		<button onclick="panHome()">Pan to Home</button>
+
+		<button onclick="panLondon()">Pan to London</button>
+
+		<button onclick="panNY()">Pan to NY</button>
+
+		<button onclick="opacityLower()">Lower Opacity</button>
+
+		<button onclick="opacityHigher()">Higher Opacity</button>
+	</p>
+
+
+	 <div id="map" class="map"></div>
+
+
+	<footer>
+		<p>This tutorial is based on code from: <a href="http://openlayers.org/en/latest/examples/animation.html">Open Layers View Animation</a></p>
+	</footer>
+</body>
+</html>
